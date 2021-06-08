28714fe8460d6530f0ed0966af72a546f17061ff
Yuri Halushko <36885543+snaipp1@users.noreply.github.com>
2021-06-08 20:34:28 +0300

    refactor: change configuration file

--- keys/index.js
+++ keys/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT : 5000,
-  MONGODB_URI: 'mongodb+srv://snaipp1:rsclone@cluster0.ajqdx.mongodb.net/rsclone?retryWrites=true&w=majority',
+  MONGODB_URI: '********************************',
   SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'snaipp1@yandex.by',
-  BASE_URL: 'http://178.124.164.50:8111'
+  SENDGRID_API_KEY: '*********',
+  EMAIL_FROM: '***********',
+  BASE_URL: '*******'
 }
